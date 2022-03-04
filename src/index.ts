3eda3116611a3883552078f7960904ca8522ba16
Kleiton Pereira <kleitonrpereira@gmail.com>
2022-03-04 00:09:40 -0300

    desafio 06 concluído

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,41 @@
-/**
- * Required External Modules
- */
 
-import * as dotenv from "dotenv";
-import express from "express";
-import cors from "cors";
-import helmet from "helmet";
+ import * as dotenv from "dotenv";
+ import express from "express";
+ import cors from "cors";
+ import helmet from "helmet";
+ const readline = require('readline-sync');
+ const db = require('../models/index.js')
+ 
+ dotenv.config();
 
-dotenv.config();
-
-/**
- * App Variables
- */
-
-if (!process.env.PORT) {
-	process.exit(1);
-}
-
-const PORT: number = parseInt(process.env.PORT as string, 10);
-
-const app = express();
-
-/**
- *  App Configuration
- */
-
-app.use(helmet());
-app.use(cors());
-app.use(express.json());
-
-/**
- * Server Activation
- */
-
-app.listen(PORT, () => {
-	console.log(`Listening on port ${PORT}`);
-
-
-	// CÓDIGO PARA ATENDER OS REQUERIMENTOS
-	// R01, R02, R03, R04, R05
-	
-});
+ 
+ if (!process.env.PORT) {
+	 process.exit(1);
+ }
+ 
+ const PORT: number = parseInt(process.env.PORT as string, 10);
+ 
+ const app = express();
+ 
+ 
+ app.use(helmet());
+ app.use(cors());
+ app.use(express.json());
+ 
+ 
+ app.listen(PORT, async () => {
+	 console.log(`Listening on port ${PORT}`);
+ 
+	 const qtd = parseInt(readline.question('Qual a quantidade de alunos? '));
+	 
+	 for (let i = 0; i < qtd; i++) {
+		  
+		 const nome = readline.question('Qual o nome do aluno? ');
+ 
+		 const nota = parseFloat(readline.question('Qual a nota do aluno? '));
+ 
+		 
+		 await db.Aluno.create({nome: nome, nota: nota});
+	 }
+ 
+ });
